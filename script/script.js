eb05e6cab0188c84a7305bc3a72c172788278f4f
Jordan <yokaizin@outlook.com>
2026-03-05 12:17:45 -0300

    initial commit

---
+++ script/script.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('form');
+const input = document.getElementById('form-input');
+const alert = document.getElementById('alert');
+const items = document.getElementById('item-container');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const inputValue = input.value
+
+  if (inputValue.trim() === "") {
+      return; 
+  }
+
+  const newItem = document.createElement('li');
+  const itemDiv = document.createElement('div');
+  const itemCheckbox = document.createElement('input');
+  const itemLabel = document.createElement('label');
+  const deleteButton = document.createElement('button');
+  const deleteIcon = document.createElement('img');
+  
+  const itemId = "item-" + Date.now();
+  
+
+  itemLabel.textContent = inputValue;
+  itemLabel.htmlFor = itemId;
+
+  newItem.classList.add('flex');
+  itemCheckbox.type = 'checkbox';
+  itemCheckbox.name = itemId;
+  itemCheckbox.id = itemId;
+
+  itemDiv.classList.add('flex');
+  items.classList.add('flex');
+
+  deleteButton.classList.add('delete-button');
+  deleteButton.type = 'button';
+
+  deleteIcon.src = 'assets/Frame.png';
+
+  deleteButton.append(deleteIcon);
+  itemDiv.append(itemCheckbox, itemLabel);
+
+  newItem.append(itemDiv, deleteButton);
+  items.append(newItem);
+  input.value = "";
+
+});
+
+items.addEventListener('click', function (event) {
+  if (event.target.closest('.delete-button')) {
+    const itemToDelete = event.target.closest('li');
+    itemToDelete.remove();
+  }
+});
