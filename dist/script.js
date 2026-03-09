60f031578fe0cfa397fbca3fcd3c1bbe87a6fcb6
Jordan <yokaizin@outlook.com>
2026-03-09 07:31:41 -0300

    babel config

--- dist/script.js
+++ dist/script.js
@@ -1,62 +1,49 @@
-const form = document.querySelector('form');
-const input = document.getElementById('form-input');
-const alert = document.getElementById('alert');
-const items = document.getElementById('item-container');
+"use strict";
 
+var form = document.querySelector('form');
+var input = document.getElementById('form-input');
+var alert = document.getElementById('alert');
+var items = document.getElementById('item-container');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  const inputValue = input.value.trim();
-
+  var inputValue = input.value.trim();
   if (inputValue === "") {
-      return; 
+    return;
   }
-
-  const newItem = document.createElement('li');
-  const itemDiv = document.createElement('div');
-  const itemCheckbox = document.createElement('input');
-  const itemLabel = document.createElement('label');
-  const deleteButton = document.createElement('button');
-  const deleteIcon = document.createElement('img');
-  
-  const itemId = "item-" + Math.random();
-  
-
+  var newItem = document.createElement('li');
+  var itemDiv = document.createElement('div');
+  var itemCheckbox = document.createElement('input');
+  var itemLabel = document.createElement('label');
+  var deleteButton = document.createElement('button');
+  var deleteIcon = document.createElement('img');
+  var itemId = "item-" + Math.random();
   itemLabel.textContent = inputValue;
   itemLabel.htmlFor = itemId;
-
   newItem.classList.add('flex');
   itemCheckbox.type = 'checkbox';
   itemCheckbox.name = itemId;
   itemCheckbox.id = itemId;
-
   itemDiv.classList.add('flex');
   items.classList.add('flex');
-
   deleteButton.classList.add('delete-button');
   deleteButton.type = 'button';
-
   deleteIcon.src = 'assets/Frame.png';
-
   deleteButton.append(deleteIcon);
   itemDiv.append(itemCheckbox, itemLabel);
-
   newItem.append(itemDiv, deleteButton);
   items.append(newItem);
   input.value = "";
-
 });
-
 items.addEventListener('click', function (event) {
   if (event.target.closest('.delete-button')) {
-    const itemToDelete = event.target.closest('li');
+    var itemToDelete = event.target.closest('li');
     itemToDelete.remove();
     alert.style.display = 'flex';
-    setTimeout(() => {
+    setTimeout(function () {
       alert.style.display = 'none';
     }, 10000);
   }
 });
-
 alert.querySelector('button').addEventListener('click', function () {
   alert.style.display = 'none';
 });
